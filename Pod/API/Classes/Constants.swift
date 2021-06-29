6e709bdbf5aff006d1fb8a707fcdabd6552cb7d7
Roman Hlebushkin <xlebushkin89@yandex.ru>
2021-06-29 13:11:12 -0400

    Add one more

--- Pod/API/Classes/Constants.swift
+++ Pod/API/Classes/Constants.swift
@@ -4,5 +4,6 @@ extension Something {
 
 	public enum MoreMore {
 		public static let foobarFoobarevichSomethingMore: FooBar = "FoobarFoobarevichSomethingMore"
+		public static let somethingThatINeedToFindReallyBad: FooBar = "SomethingThatINeedToFindReallyBad"
 	}
 }
